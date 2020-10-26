0115eb04ed2bc27a0aedf24e5a172f546d7f6d20
Emerson <emerson.santana@solutis.com.br>
2020-10-26 09:33:32 -0300

    Route - Configuring basic route

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home(){
+  return(
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  )
+}
